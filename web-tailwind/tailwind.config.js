d37f298733650cecc33b36bf68990ea8e437173b
Anderson Mena <amserna@stefanini.com>
2023-05-25 23:58:42 -0500

    Finalización animaciones header

--- web-tailwind/tailwind.config.js
+++ web-tailwind/tailwind.config.js
@@ -6,12 +6,31 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'xs': {'max': '640px'}
+      },
       backgroundImage: theme => ({
         'body-pattern': 'url(../img/pattern.png);'
       }),
       colors: {
         'blue-light': '#37bcf9',
         'blue-dark': '#0370b9'
+      },
+      animation: {
+        'spin-low': 'spin 2s linear infinite',
+        'from-bellow': 'fromBellow 500ms linear',
+        'from-right': 'fromRight 300ms linear'
+      },
+      keyframes: {
+        fromBellow: {
+          '0%': { transform: 'translateY(0%)'},
+          '50%': { transform: 'translateY(200%)'},
+          '100%': { transform: 'translateY(0%)'}
+        },
+        fromRight: {
+          '0%': { transform: 'translateX(200%)'},
+          '100%': { transform: 'translateX(0%)'}
+        }
       }
     },
   },
